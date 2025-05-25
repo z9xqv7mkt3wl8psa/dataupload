f1260225c13e7d776ea647f8599a380d21ec7168
Prasunet <162030269+z9xqv7mkt3wl8psa@users.noreply.github.com>
2025-05-25 16:25:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Important for App Router projects
+  },
 };
 
 export default nextConfig;
